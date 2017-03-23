2bc7424e48c772fefd5a745324059fab684b2acd
Thomas Conte <Thomas.Conte@ensimag.grenoble-inp.fr>
2017-03-23 16:27:35 +0100

    modif

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var chatservice = require('../server/chatService.js')
+var chatservice = require('../server/chatService.js');
 
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
