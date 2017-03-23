6fabad4ffaf2cf61750e1b5fb75d462448cd1efd
Thomas Conte <Thomas.Conte@ensimag.grenoble-inp.fr>
2017-03-23 15:52:30 +0100

    modif

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var chatservice = require('../server/chatService.js')
 
 /* GET hello world page. */
-router.get('/', function(req, res, next) {
+router.get('/webhook', function(req, res, next) {
 	if (chatservice.authenticate(req)){
 	    res.status(200).send(req.query['hub.challenge']);
 	} else {
