161728894ffe9ff32f1e7840681abab0a7364561
Thomas Conte <Thomas.Conte@ensimag.grenoble-inp.fr>
2017-03-23 16:01:28 +0100

    modif

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var homepage = require('./routes/index');
 
 var app = express();
+var webhook = requiert('./routes/webhook');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
