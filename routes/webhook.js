3b3814ed793289096053bf1923cbe327dc00e053
Thomas Conte <Thomas.Conte@ensimag.grenoble-inp.fr>
2017-03-23 15:42:34 +0100

    modif

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var chatservice = require('../server/chatService.js')
 
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
+	authenticate(req);
   res.render('index');
 });
 
