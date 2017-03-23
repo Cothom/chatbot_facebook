329fbdc3a05db72fce281a3190e2cabb9f55b8c1
Thomas Conte <Thomas.Conte@ensimag.grenoble-inp.fr>
2017-03-23 16:57:36 +0100

    modif

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var chatservice = require('../server/chatService.js');
+var userservice = require('../server/userService.js');
 
 /* GET hello world page. */
 router.get('/', function(req, res, next) {
@@ -14,23 +15,30 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   var data = req.body;
 
-  // Make sure this is a page subscription
-  if (data.object === 'page') {
+  if (userservice.isUserKnown(data.entry.id)){
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function(entry) {
+          var pageID = entry.id;
+          var timeOfEvent = entry.time;
 
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.message) {
-          chatservice.receivedMessage(event);
-        } else {
-          console.log("Webhook received unknown event: ", event);
-        }
-      });
-    });
-}});
+          // Iterate over each messaging event
+          entry.messaging.forEach(function(event) {
+            if (event.message) {
+              chatservice.receivedMessage(event);
+            } else {
+              console.log("Webhook received unknown event: ", event);
+            }
+          });
+        });
+    }
+   } else {
+       userService.addUser(data.entry.id, data);
+       chatService.sendTextMessage(data.entry.id, "Bienvenue !");
+   }
+   res.status(200).send(req.query['hub.challenge']);
+});
 
 module.exports = router;
